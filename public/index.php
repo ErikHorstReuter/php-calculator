fbeb2126148d4a2d5d0464254b5f3dcf5a9727ac
Erik Horst Reuter <erik.horst.reuter@outlook.de>
2025-12-26 02:02:34 +0100

    added index for planning

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Calculator\BilloDiContainer\BilloDiContainer;
+use Calculator\Calculator\CalculatorInterface;
+use Calculator\IO\IOInterface;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+/** @var BilloDiContainer $container */
+$container = require_once __DIR__ . '/../src/BilloDiContainer/mapping.php';
+
+/** @var IOInterface $console */
+$console = $container->instance(IOInterface::class);
+/** @var CalculatorInterface $calculator */
+$calculator = $container->instance(CalculatorInterface::class);
+
+
+$console->writeLine('=== PHP Console - Calculator ===');
+
+$loop = true;
+do {
+
+    $term = $console->readLine('Bitte gib deine Term jetzt ein: ');
+
+    try {
+        $result = $calculator->calculate($term);
+    } catch (Exception $e) {
+        $console->writeLine('Der Term konnte leider nicht berechnet werden.');
+    }
+
+    $console->writeLine("Ergebnis: $result");
+
+    $loop = $console->readYesNo('Möchtest du einen weiteren Term berechnen?');
+
+} while ($loop);
+
+$console->writeLine('Bis zum nächsten Mal!');
